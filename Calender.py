300faaecf2ef2b127576e02450a76f510fa084c3
Satyam Kumar <98641231+satyamkumar420@users.noreply.github.com>
2023-08-01 23:54:14 +0530

    Update Calender.py

--- Calender.py
+++ Calender.py
@@ -9,20 +9,20 @@ def date_show():
     my_label.config(text=f"Today's Date Is: {cal.get_date()}", fg="blue",)
 
 
-# try:
-# TODO: realtime date 
-cal = Calendar(root, selectmode="day", )
-cal.pack(pady=20, fill="both", expand=True)
+try:
+    # TODO: realtime date 
+    cal = Calendar(root, selectmode="day", )
+    cal.pack(pady=20, fill="both", expand=True)
 
-my_label = Label(root, text="", font=("Arial", 20), fg="white")
-my_label.pack(pady=10)
+    my_label = Label(root, text="", font=("Arial", 20), fg="white")
+    my_label.pack(pady=10)
 
-my_button = Button(root, text='Select Date', command=date_show,
-                bg='blue', fg='white', font=("Arial", 17))
-my_button.pack(pady=20)
+    my_button = Button(root, text='Select Date', command=date_show,
+                    bg='blue', fg='white', font=("Arial", 17))
+    my_button.pack(pady=20)
 
-root.mainloop()
-
-# except KeyboardInterrupt:
-#     print("\n \033[01;31m Thanks! For Using 😉")
+    root.mainloop()
+    
+except KeyboardInterrupt:
+    print("\n \033[01;31m Thanks! For Using 😉")
     
